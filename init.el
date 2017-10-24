7c4f116a7709552d79752394ec57b60cf84c8e6a
lsytj0413 <511121939@qq.com>
2017-10-24 11:16:25 +0800

    exec-path-from-shell-check-startup-files nil

--- init.el
+++ init.el
@@ -318,6 +318,7 @@ executes.
  This function is mostly useful for variables that need to be set
 before packages are loaded. If you are unsure, you should try in setting them in
 `dotspacemacs/user-config' first."
+  (setq exec-path-from-shell-check-startup-files nil)
   (setenv "LC_CTYPE" "zh_CN.UTF-8")
   (cd (concat (getenv "HOME")
               "/"))
